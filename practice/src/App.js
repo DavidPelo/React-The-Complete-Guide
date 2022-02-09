c9f9e82caaa99146c9d64612a41a16db2a541df7
davidwpelo <65370511+davidwpelo@users.noreply.github.com>
2022-02-09 08:27:22 -0600

    completed practice project

--- practice/src/App.js
+++ practice/src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import AddUser from './components/Users/AddUser'
+import UsersList from './components/Users/UsersList';
+
 
 function App() {
+  const [usersList, setUsersList] = useState([]);
+
+  const addUserHandler = (username, age) => {
+    const newUser = {
+      name: username,
+      age: age,
+      id: Math.random() * 100
+    }
+
+    setUsersList([...usersList, newUser]);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <AddUser onUserAdd={addUserHandler}/>
+      <UsersList users={usersList}/>
+
     </div>
   );
 }
