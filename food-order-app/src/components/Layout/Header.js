b4bbea6ea8497d0d22d2cd9de8e9e8fc6e46f2c0
davidwpelo <65370511+davidwpelo@users.noreply.github.com>
2022-02-24 09:00:48 -0600

    Added modal  portal and implemented cart UI

--- food-order-app/src/components/Layout/Header.js
+++ food-order-app/src/components/Layout/Header.js
@@ -4,12 +4,12 @@ import styles from './Header.module.css'
 import mealsImage from '../../assets/meals.jpg'
 import HeaderCartButton from './HeaderCartButton'
 
-function Header() {
+function Header(props) {
   return (
     <>
       <header className={styles.header}>
         <h1>ReactMeals</h1>
-        <HeaderCartButton />
+        <HeaderCartButton onClick={props.onShowCart}/>
       </header>
       <div className={styles['main-image']}>
         <img src={mealsImage} alt="A table full of delicious food!" />
